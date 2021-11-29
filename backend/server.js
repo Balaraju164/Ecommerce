93a145820e2d8f5f6660024755a2328b7e9bff3e
Balaraju164 <balaraju164570@gmail.com>
2021-11-29 09:32:01 +0530

    deployment chages completed

--- backend/server.js
+++ backend/server.js
@@ -25,13 +25,17 @@ app.use('/api/upload',uploadRoutes)
 const directory = path.resolve(__dirname,'/frontend/public/images')
 app.use('/uploads',express.static(path.join(directory)))
 
-if(process.env.NODE_ENV==='production')
-{
-    app.use(express.static(path.join(__dirname,'/frontend/build')))
-    app.get('*',(req,res)=>{res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))})
-}else{
-    app.get('/',(req,res)=>{res.send('Bala is running....')})
-}
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(path.resolve(__dirname,'..', 'frontend', 'build', 'index.html'))
+    )
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....')
+    })
+  }
 
 //Middleware -1
 app.use(notFound)
